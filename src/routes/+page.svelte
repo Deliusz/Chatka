c45c3d494717dab23db834d146c32b5a86896647
Deliusz <mateuszwyszogrodzki10@gmail.com>
2023-08-20 22:03:37 +0200

    Added title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1 class="text-3xl font-bold underline dark:bg-amber-200">Welcome to SvelteKit</h1>
+<head>
+    <title>Chatka</title>
+</head>
+
+<h1 class="text-3xl font-bold underline dark:bg-emerald-700 rounded-b-lg text-center py-2">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
