8b984f6165e0e36116baf8a631b45ce12473edc7
Deliusz <mateuszwyszogrodzki10@gmail.com>
2023-08-21 01:46:19 +0200

    Added basic header

---
+++ src/lib/header.svelte
@@ -0,0 +1,12 @@
+<script>
+	import UserDropdown from '$lib/userDropdown.svelte';
+</script>
+
+<header class="h-14 w-screen flex bg-neutral-700 p-2 rounded-b-lg">
+	<a href="/" class="w-10 flex">
+		<img class="h-10" src="icon/favicon.svg" alt="Logo" />
+	</a>
+	<div class="ml-auto">
+		<UserDropdown />
+	</div>
+</header>
---
+++ src/lib/userDropdown.svelte
@@ -0,0 +1,25 @@
+<!-- component -->
+<div class="w-60">
+	<div class="group w-max ml-auto">
+		<button class=" outline-none focus:outline-none w-auto h-10 ml-auto flex">
+			<span class="w-auto h-10 text-1xl mt-2 mr-2 block"> Username </span>
+			<span class="h-10 w-10 inline-block rounded-full overflow-hidden">
+				<i class="fa-solid fa-user text-4xl" />
+			</span>
+		</button>
+		<div
+			class="w-full absolute left-0 sm:left-auto sm:right-0 invisible group-hover:visible sm:w-40 transform opacity-0 group-hover:opacity-100
+            transition duration-300 ease-in-out origin-top"
+		>
+			<ul class=" bg-neutral-700 rounded-b-lg pt-2 text-center">
+				<li class="rounded-sm px-3 py-2 hover:bg-neutral-600">Programming</li>
+				<li class="rounded-sm px-3 py-2 hover:bg-neutral-600">DevOps</li>
+				<li class="px-3 py-2 hover:bg-neutral-600">Go</li>
+				<li class="px-3 py-2 hover:bg-neutral-600">Rust</li>
+				<li class="rounded-b-lg px-3 py-2 hover:bg-neutral-600">Testing</li>
+			</ul>
+		</div>
+	</div>
+</div>
+<!-- rounded-b collapse group-hover:visible transform opacity-0 left-0 group-hover:opacity-100
+    transition duration-200 ease-in-out -->
