73fe9d70ac4abcba0d289ee74960a210dad3f234
Deliusz <mateuszwyszogrodzki10@gmail.com>
2023-08-21 01:47:29 +0200

    Added title to login and home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <head>
-    <title>Chatka</title>
+	<title>Chatka</title>
 </head>
-
-<h1 class="text-3xl font-bold underline dark:bg-emerald-700 rounded-b-lg text-center py-2">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- <h1 class="text-3xl font-bold underline dark:bg-emerald-700 rounded-b-lg text-center py-2">Welcome to SvelteKit</h1> -->
+<!-- <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
