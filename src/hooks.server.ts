f0c813e24fb89cd7f7c18ec995a96584eea5636a
Deliusz <mateuszwyszogrodzki10@gmail.com>
2023-09-04 11:26:35 +0200

    Added hooks for auth

---
+++ src/hooks.server.ts
@@ -0,0 +1,31 @@
+// src/hooks.server.js
+import PocketBase from 'pocketbase';
+
+/** @type {import('@sveltejs/kit').Handle} */
+export async function handle({ event, resolve }) {
+	// event.locals.pb = new PocketBase('http://127.0.0.1:8090');
+	event.locals.pb = new PocketBase('http://10.100.100.1:81');
+
+	// load the store data from the request cookie string
+	event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
+
+	try {
+		// get an up-to-date auth store state by verifying and refreshing the loaded auth model (if any)
+		event.locals.pb.authStore.isValid && (await event.locals.pb.collection('users').authRefresh());
+	} catch (_) {
+		// clear the auth store on failed refresh
+		event.locals.pb.authStore.clear();
+	}
+
+	const response = await resolve(event);
+
+	// send back the default 'pb_auth' cookie to the client with the latest store state
+
+	//TODO: SECURE IN PRODUCTION
+	response.headers.append(
+		'set-cookie',
+		event.locals.pb.authStore.exportToCookie({ secure: false })
+	);
+
+	return response;
+}
