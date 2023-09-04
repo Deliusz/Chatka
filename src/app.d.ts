d324603a6148f9770c84ff881d19660288496930
Deliusz <mateuszwyszogrodzki10@gmail.com>
2023-09-04 11:26:17 +0200

    Added pocketbase types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,11 +1,17 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+// src/app.d.ts
+import PocketBase from 'pocketbase';
 declare global {
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
 		// interface Platform {}
+		interface Locals {
+			pb: PocketBase;
+			user
+		}
 	}
 }
 
