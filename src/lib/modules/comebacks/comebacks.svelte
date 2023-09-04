d607c6fdd5c888f152b8741bae9f665792102b6f
Deliusz <mateuszwyszogrodzki10@gmail.com>
2023-09-04 21:55:58 +0200

    Added comebacks module

---
+++ src/lib/modules/comebacks/comebacks.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+	import ComebacksBlock from './comebacksBlock.svelte';
+	import { onMount } from 'svelte';
+
+	interface timeData {
+		Mon: string;
+		Tue: string;
+		Wed: string;
+		Thu: string;
+		Fri: string;
+		Sat: string;
+		Sun: string;
+	}
+	interface ComebacksData {
+		username: string;
+		avatar: string;
+		time: timeData;
+	}
+
+	let dataComplete = false;
+	let comebacks: Array<ComebacksData>;
+
+	onMount(async () => {
+		let interval = setInterval(async () => {
+			const temps = await fetch('http://10.100.100.1:5173/api/comebacks');
+			const json = await temps.json();
+			comebacks = json;
+			const currnetDate = new Date();
+			const currentDay = currnetDate.toDateString().split(' ')[0];
+			comebacks.sort((a, b) => {
+				let aa = new Date(`2000-01-01T${a.time[currentDay as keyof timeData]}`).getTime();
+				let bb = new Date(`2000-01-01T${b.time[currentDay as keyof timeData]}`).getTime();
+
+				return aa - bb;
+			});
+
+			dataComplete = true;
+		}, 1000);
+	});
+</script>
+
+{#if dataComplete}
+	<div
+		class="bg-neutral-800 p-4 w-100 sm:w-96 rounded-2xl"
+		in:fade={{ duration: 500 }}
+		out:fade={{ duration: 500 }}
+	>
+		<span class="text-lg font-semibold mb-4 block sm:text-left">Comebacks</span>
+		<div class=" flex flex-wrap flex-col gap-3 justify-center">
+			{#each comebacks as comeback}
+				{#key comeback}
+					<ComebacksBlock
+						avatar={comeback.avatar}
+						username={comeback.username}
+						time={comeback.time}
+					/>
+				{/key}
+			{/each}
+		</div>
+	</div>
+{/if}
---
+++ src/lib/modules/comebacks/comebacksBlock.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	interface timeData {
+		Mon: string;
+		Tue: string;
+		Wed: string;
+		Thu: string;
+		Fri: string;
+		Sat: string;
+		Sun: string;
+	}
+	export let username: string;
+	export let time: timeData;
+	export let avatar: string;
+	avatar = avatar == '' ? '/defaultUserAvatar.png' : avatar;
+	const currnetDate = new Date();
+	const currentDay = currnetDate.toDateString().split(' ')[0];
+
+	let timeCurrentDay = time[currentDay as keyof timeData];
+</script>
+
+{#if time && timeCurrentDay != '00:00'}
+	<div class="bg-gradient-to-tr from-teal-500 to-emerald-700 flex flex-row w-100 p-3 rounded-2xl">
+		<img class="h-10 mr-2 rounded-full" src={avatar} alt="" />
+		<span class="text-2xl font-semibold self-center">{username}</span>
+		<span class="text-3xl font-bold self-center ml-auto mr-0">{timeCurrentDay}</span>
+	</div>
+{/if}
