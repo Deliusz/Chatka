3026a31cd71b88326c8e9fedad5139d1386dcbed
Deliusz <mateuszwyszogrodzki10@gmail.com>
2023-09-04 22:05:45 +0200

    Added animations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,20 @@
 <script>
 	import TemperatureModule from '$lib/modules/temperature/temperatures.svelte';
+	import ComebacksModule from '$lib/modules/comebacks/comebacks.svelte';
+	import { fly, fade } from 'svelte/transition';
 </script>
 
 <head>
 	<title>Chatka</title>
 </head>
 
-<TemperatureModule />
+<div
+	out:fly={{ x: 400 }}
+	in:fly={{ x: -400 }}
+	class="justify-center sm:justify-normal sm:w-100 flex gap-2 flex-col sm:flex-row p-2"
+>
+	<TemperatureModule />
+	<ComebacksModule />
+</div>
+<a href="/test">Test</a>
+<a href="/">Home</a>
