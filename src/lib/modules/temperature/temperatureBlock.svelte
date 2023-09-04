d2dfa2c7db945b831133effc50bbd7cb01df8c39
Deliusz <mateuszwyszogrodzki10@gmail.com>
2023-09-04 21:57:43 +0200

    Added animations and changed temp and state variables to functions

--- src/lib/modules/temperature/temperatureBlock.svelte
+++ src/lib/modules/temperature/temperatureBlock.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
+	import { fade } from 'svelte/transition';
 	export let color: string;
 	export let temperature: string;
 	export let name: string;
 	export let breakPoints = [40, 45, 50, 55];
-	let temp = Number(temperature) || 0;
-	let state =
-		temp >= breakPoints[3]
+
+	let state = (temperature: string) => {
+		let temp = Number(temperature);
+		return temp >= breakPoints[3]
 			? 'full'
 			: temp >= breakPoints[2]
 			? 'three-quarters'
@@ -14,12 +16,18 @@
 			: temp >= breakPoints[0]
 			? 'quarter'
 			: 'empty';
+	};
+	let animate = (temperature: string) => {
+		return temperature == '0' ? 'animate-pulse' : '';
+	};
 </script>
 
-<div class="{color}  sm:w-40 basis-[48%] p-3 rounded-2xl">
+<div class="{color} sm:w-40 basis-[48%] p-3 rounded-2xl" in:fade={{ duration: 500 }}>
 	<div class="mb-2">
-		<i class="fa-solid fa-temperature-{state} text-4xl" />
-		<span class=" float-right text-4xl"><b>{temperature}</b>°C</span>
+		{#key temperature}
+			<i class="fa-solid fa-temperature-{state(temperature)} {animate(temperature)}  text-4xl" />
+		{/key}
+		<span class=" float-right text-4xl {animate(temperature)} "><b>{temperature}</b>°C</span>
 	</div>
-	<span class="font-semibold">{name}</span>
+	<span class="font-semibold {animate(temperature)} ">{name}</span>
 </div>
