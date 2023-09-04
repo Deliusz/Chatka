02d9c504442907d8a318b1e46e0d77c388d912ba
Deliusz <mateuszwyszogrodzki10@gmail.com>
2023-09-04 11:28:38 +0200

    Added temperature module to home screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
+<script>
+	import TemperatureModule from '$lib/modules/temperature/temperatures.svelte';
+</script>
+
 <head>
 	<title>Chatka</title>
 </head>
-<!-- <h1 class="text-3xl font-bold underline dark:bg-emerald-700 rounded-b-lg text-center py-2">Welcome to SvelteKit</h1> -->
-<!-- <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+
+<TemperatureModule />
