064989771262562b86327bc6def97272b0a57260
Deliusz <mateuszwyszogrodzki10@gmail.com>
2023-09-08 21:54:28 +0200

    Added color to return

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,4 +1,4 @@
-import { redirectIfUserIsNotLoggedIn } from '$lib/helpers.js';
+import { redirectIfUserIsNotLoggedIn } from '$lib/functions/helpers';
 
 export const load = async ({ locals, url }) => {
 	if (url.pathname != '/login') redirectIfUserIsNotLoggedIn(locals, '/login');
@@ -10,7 +10,8 @@ export const load = async ({ locals, url }) => {
 				avatar: locals.pb.files.getUrl(locals.pb.authStore.model, locals.pb.authStore.model.avatar),
 				email: locals.pb.authStore.model.email,
 				name: locals.pb.authStore.model.name,
-				username: locals.pb.authStore.model.username
+				username: locals.pb.authStore.model.username,
+				color: locals.pb.authStore.model.accentColor
 			}
 		};
 	}
